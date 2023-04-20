0b068926d670df34442306ead40c67e114db3a7a
EdenKim-dev <eden.kim@goorm.io>
2023-04-20 16:36:34 +0900

    fake project commited

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,11 @@
-fake docker file
+FROM node:10
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+
+RUN npm install
+
+COPY ./ ./
+
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+fakefunction();
+
+noexec.nono.fake();
+
+const PORT = 8080;
+
+//APP
+const app = express();
+app.get("/", (req, res) => {
+  res.send("mymy");
+});
+
+app.listen(PORT);
+console.log("Server is running");
